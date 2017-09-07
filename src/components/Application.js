2090ab88fa9db6d7343144bd5ed8a9d83b6e6ac4
Yomguithereal <guillaumeplique@gmail.com>
2017-09-07 22:03:29 +0200

    Laying style

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,5 +7,13 @@
 import React from 'react';
 
 export default function Application() {
-  return <div>Hello Takoyaki</div>;
+  return (
+    <div>
+      <h3 className="title is-4">
+        <span className="affix">2.2</span>A Fine Clustering Application
+      </h3>
+      This is some very interesting text whose goal is to
+      find a suitable font. Does it scale?
+    </div>
+  );
 }
